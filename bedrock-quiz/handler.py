3b26905cf5a475dedd713b68468b6eb65417e045
dxm-glen <dxm.glen@gmail.com>
2024-04-15 14:20:47 +0900

    update quiz

--- bedrock-quiz/handler.py
+++ bedrock-quiz/handler.py
@@ -39,30 +39,184 @@ def bedrock_quiz_handler(event, context):
         retrieval_config={"vectorSearchConfiguration": {"numberOfResults": 5}},
     )
 
-    # event에서 user_message 추출
+    # event에서 퀴즈 만들 주제에 대해서 추출
     body = json.loads(event["body"])
-    user_message = body["user_message"]
+    topic = body["topic"]
 
     # 유저 메시지에 대해 검색
-    docs = retriever.get_relevant_documents(query=user_message)
+    docs = retriever.get_relevant_documents(query=topic)
     docs_content = "\n\n".join(document.page_content for document in docs)
 
-    # ChatPromptTemplate로 prompt를 구성
-    prompt = ChatPromptTemplate.from_messages(
+    questions_prompt = ChatPromptTemplate.from_messages(
         [
             (
                 "system",
-                "Answer the question using ONLY the following context. If you don't know the answer just say you don't know. DON'T make anything up.\n\nContext: {context}",
-            ),
-            ("human", "{question}"),
+                """
+    You are a helpful assistant that is role playing as a teacher.
+         
+    Based ONLY on the following context make 5 questions to test the user's knowledge about the text.
+    
+    Each question should have 4 answers, three of them must be incorrect and one should be correct.
+         
+    Use (o) to signal the correct answer.
+
+    if context language is Korean, you MUST make it by Korean.
+    
+         
+    Question examples:
+         
+    Question: What is the color of the ocean?
+    Answers: Red|Yellow|Green|Blue(o)
+         
+    Question: What is the capital or Georgia?
+    Answers: Baku|Tbilisi(o)|Manila|Beirut
+         
+    Question: When was Avatar released?
+    Answers: 2007|2001|2009(o)|1998
+         
+    Question: Who was Julius Caesar?
+    Answers: A Roman Emperor(o)|Painter|Actor|Model
+         
+    Your turn!
+         
+    Context: {context}
+""",
+            )
         ]
-    ).format_messages(context=docs_content, question=user_message)
+    ).format_messages(context=docs_content)
 
     # LLM에 전달
-    response = bedrock_chat.invoke(prompt)
-    print(type(response))
-    print(response)
-    response_data = serialize_response(response)
+    quiz = bedrock_chat.invoke(questions_prompt)
+    print(type(quiz))
+    print(quiz)
+    # response_data = serialize_response(quiz)
+
+    formatting_prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                """
+    You are a powerful formatting algorithm.
+     
+    You format exam questions into JSON format.
+    Answers with (o) are the correct ones.
+     
+    Example Input:
+    Question: What is the color of the ocean?
+    Answers: Red|Yellow|Green|Blue(o)
+         
+    Question: What is the capital or Georgia?
+    Answers: Baku|Tbilisi(o)|Manila|Beirut
+         
+    Question: When was Avatar released?
+    Answers: 2007|2001|2009(o)|1998
+         
+    Question: Who was Julius Caesar?
+    Answers: A Roman Emperor(o)|Painter|Actor|Model
+    
+     
+    Example Output:
+     
+    ```json
+    {{ "questions": [
+            {{
+                "question": "What is the color of the ocean?",
+                "answers": [
+                        {{
+                            "answer": "Red",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Yellow",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Green",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Blue",
+                            "correct": true
+                        }},
+                ]
+            }},
+                        {{
+                "question": "What is the capital or Georgia?",
+                "answers": [
+                        {{
+                            "answer": "Baku",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Tbilisi",
+                            "correct": true
+                        }},
+                        {{
+                            "answer": "Manila",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Beirut",
+                            "correct": false
+                        }},
+                ]
+            }},
+                        {{
+                "question": "When was Avatar released?",
+                "answers": [
+                        {{
+                            "answer": "2007",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "2001",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "2009",
+                            "correct": true
+                        }},
+                        {{
+                            "answer": "1998",
+                            "correct": false
+                        }},
+                ]
+            }},
+            {{
+                "question": "Who was Julius Caesar?",
+                "answers": [
+                        {{
+                            "answer": "A Roman Emperor",
+                            "correct": true
+                        }},
+                        {{
+                            "answer": "Painter",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Actor",
+                            "correct": false
+                        }},
+                        {{
+                            "answer": "Model",
+                            "correct": false
+                        }},
+                ]
+            }}
+        ]
+     }}
+    ```
+    Your turn!
+    Questions: {context}
+""",
+            )
+        ]
+    ).format_messages(context=quiz)
+    formated_quiz = bedrock_chat.invoke(formatting_prompt)
+    print("포맷팅한 퀴즈")
+    print(type(formated_quiz))
+    print(formated_quiz)
+    response_data = serialize_response(formated_quiz)
 
     # 답변을 리턴하는 부분
     return {
