f2b10e5de9f4fecbef09007ee4572acaf4c5f415
dxm-glen <dxm.glen@gmail.com>
2024-04-16 17:03:07 +0900

    update chat part streaming

--- bedrock-chat/handler.py
+++ bedrock-chat/handler.py
@@ -7,6 +7,8 @@ from langchain_community.chat_models import BedrockChat
 from langchain_community.retrievers import AmazonKnowledgeBasesRetriever
 from langchain.prompts import ChatPromptTemplate
 
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+
 
 def serialize_response(response):
     return {
@@ -17,7 +19,6 @@ def serialize_response(response):
 
 
 def bedrock_chat_handler(event, context):
-    # AWS 세션을 설정
     session = boto3.Session(
         aws_access_key_id=os.getenv("AWS_ACCESS_KEY"),
         aws_secret_access_key=os.getenv("AWS_SECRET_KEY"),
@@ -29,6 +30,10 @@ def bedrock_chat_handler(event, context):
     bedrock_chat = BedrockChat(
         client=bedrock_client,
         model_id="anthropic.claude-3-sonnet-20240229-v1:0",
+        #
+        streaming=True,
+        callbacks=[StreamingStdOutCallbackHandler()],
+        #
         model_kwargs={
             "temperature": 0.1,
         },
@@ -44,11 +49,9 @@ def bedrock_chat_handler(event, context):
 
     print(user_message)
 
-    # 유저 메시지에 대해 검색
     docs = retriever.get_relevant_documents(query=user_message)
     docs_content = "\n\n".join(document.page_content for document in docs)
 
-    # ChatPromptTemplate로 prompt를 구성
     prompt = ChatPromptTemplate.from_messages(
         [
             (
@@ -59,13 +62,11 @@ def bedrock_chat_handler(event, context):
         ]
     ).format_messages(context=docs_content, question=user_message)
 
-    # LLM에 전달
     response = bedrock_chat.invoke(prompt)
     print(type(response))
     print(response)
     response_data = serialize_response(response)
 
-    # 답변을 리턴하는 부분
     return {
         "statusCode": 200,
         "headers": {"Content-Type": "application/json"},
